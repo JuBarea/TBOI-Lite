b9b77d60f0663e9a8275e94d7dcfdbad41767d00
JuBarea <jubarea@ucm.es>
2017-12-01 02:05:08 +0100

    Movement(done) and Shooting(90%)
    Movement for the player is done
    Bullets are almost done, the only thing left to do is moving the BulletPool over the the bullet class creation if possible

--- src/js/BasicClass.js
+++ src/js/BasicClass.js
@@ -2,6 +2,8 @@
 function Basic(game,x,y,key){
     Phaser.Sprite.call(this,game,x,y,key);
     this.game.world.addChild(this);
+    game.physics.arcade.enable(this)
+    this.body.collideWorldBounds = true;
     
 }
 Basic.prototype = Object.create(Phaser.Sprite.prototype); //We make the Basic class inherit from the Sprite Class
@@ -21,7 +23,6 @@ Basic.prototype.place = function(x,y){
         Basic.apply(this,[game,x,y,key]);
         this.speed = speed;
     }
-
     //heritage
     Moveable.prototype = Object.create(Basic.prototype);
     Moveable.constructor = Moveable;
@@ -30,6 +31,7 @@ Basic.prototype.place = function(x,y){
             function Bullet(game,x,y,key,speed,dir){
                 Moveable.apply(this,[game,x,y,key,speed])
                 this.dir = dir;
+                this.f = this.alive;
             }
 
             Bullet.prototype = Object.create(Moveable.prototype);
@@ -48,139 +50,124 @@ Basic.prototype.place = function(x,y){
                     this.x += this.speed;
                 }
             }  
+            Bullet.prototype.resete = function(x,y,speed,dir){
+                this.reset(x,y)
+                this.speed = speed;
+                this.dir = dir;
+            }
         
             Bullet.prototype.update = function(){
-                this.move();
-                //Collision();
+                if(this.alive)this.move();
+                if(this.x > this.game.width)this.kill();
+                if(this.f != this.alive){
+                    console.log("Changing State")
+                    this.f = this.alive
+                }
             }
 
         
         ///Player
-            function Player(game,x,y,key,speed,head){
+            function Player(game,x,y,key,speed,head,tearsKey){
                 Moveable.apply(this,[game,x,y,key,speed]);
 
                 this.head = head;
-                /*var cursors;
-                var keyW;
-                var keyS;
-                var keyA;
-                var keyD;*/
-
-
-                
+                this.bulletPool = tearsKey; //Array or key??
+                this.bulletSpeed = 5;
+                var dir;
+                this.FireRate = 300;
+                this.bulletTimer = 0;
+                this.i = 0;
+                                
                 //  Our two animations, walking left, right and up/down.
                     this.animations.add('left', [20, 21, 22,23,24,25,26,27,28,29], 20, true);
                     this.animations.add('right', [10, 11, 12,13,14,15,16,17,18,19], 20, true);
                     this.animations.add('up',[0,1,2,3,4,5,6,7,8,9],20,true);
-
-                //Head Set-up
-                    //this.addChild(this.head);
-                    //this.head.place(0,0);
-                    
-
                 //Contorls
                     cursors = this.game.input.keyboard.createCursorKeys();
                     keyW = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
                     keyA = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
                     keyS = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
                     keyD = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
-        
-
-                /*this.move = function(){                  
-                
-                    if(keyA.isDown || keyD.isDown || keyW.isDown ||keyS.isDown){
-                        if (keyA.isDown)
-                        {
-                            //  Move to the left
-                            this.body.velocity.x = -150;
-                            this.animations.play('left');
-                            head.frame = 6;
-                        }
-                        if (keyD.isDown)
-                        {
-                            //  Move to the right
-                            this.body.velocity.x = 150;
-                            this.animations.play('right');
-                            head.frame = 2;
-                        } 
-                        if(keyW.isDown)
-                        {
-                            this.body.velocity.y = -150
-                            player.animations.play('up');
-                            head.frame = 4;
-                    
-                        }
-                        if(keyS.isDown)
-                        {
-                            this.body.velocity.y = 150   
-                            this.animations.play('up');
-                            head.frame = 0;
-                    
-                        }
-                
-                    }
-                    else{
-                        //  Reset the players velocity (movement)
-                        this.animations.stop();
-                        this.frame = 0;
-                        head.animations.stop();
-                        head.frame = 0;
-                    }
-                }*/
-
-
             }
 
             Player.prototype = Object.create(Moveable.prototype);
             Player.constructor = Player;
             Player.prototype.move = function(){ 
-                
-                    if(keyA instanceof Phaser.Key)
-                        console.log("Ou yeah baby");
-                
+          
                     if(keyA.isDown || keyD.isDown || keyW.isDown ||keyS.isDown){
-                        if (this.keyA.isDown)
+                        if (keyA.isDown)
                         {
                             //  Move to the left
-                            this.x -= -speed;
+                            this.x -= this.speed;
                             this.animations.play('left');
-                            head.frame = 6;
+                            this.head.frame = 6;
                         }
-                        if (this.keyD.isDown)
+                        if (keyD.isDown)
                         {
                             //  Move to the right
-                            this.x += speed;
+                            this.x += this.speed;
                             this.animations.play('right');
-                            head.frame = 2;
+                            this.head.frame = 2;
                         } 
-                        if(this.keyW.isDown)
+                        if(keyW.isDown)
                         {
-                            this.y -= speed;
-                            player.animations.play('up');
-                            head.frame = 4;
+                            this.y -= this.speed;
+                            this.animations.play('up');
+                            this.head.frame = 4;
+                         
                     
                         }
-                        if(this.keyS.isDown)
+                        if(keyS.isDown)
                         {
-                            this.y += speed   
+                            this.y += this.speed   
                             this.animations.play('up');
-                            head.frame = 0;
-                    
+                            this.head.frame = 0;
+                           
                         }
                 
                     }
                     else{
                         //  Reset the players velocity (movement)
                         this.animations.stop();
-                        this.frame = 0;
-                        //head.animations.stop();
-                        head.frame = 0;
+                        this.frame = 0;  
+                        this.head.frame = 0;
+                        
                     }
             }         
+            Player.prototype.shoot = function(){
+
+                if(cursors.left.isDown || cursors.right.isDown || cursors.up.isDown ||cursors.down.isDown ){
+                    if(this.game.time.now > this.bulletTimer){
+                        if(this.i >= 10) this.i = 0;
+                        if (cursors.left.isDown)
+                        {
+                            this.bulletPool[this.i].resete(this.x,this.y,this.bulletSpeed,'left')   
+                            this.i++ 
+                    
+                        }
+                        else if (cursors.right.isDown)
+                        {
+                            this.bulletPool[this.i].resete(this.x,this.y,this.bulletSpeed,'right') 
+                            this.i++
+                           
+                        } 
+                        else if(cursors.up.isDown)
+                        {
+                            this.bulletPool[this.i].resete(this.x,this.y,this.bulletSpeed,'up') 
+                            this.i++                                                                 
+                        } else if(cursors.down.isDown)
+                        {   
+                            this.bulletPool[this.i].resete(this.x,this.y,this.bulletSpeed,'down') 
+                            this.i++           
+
+                        }
+                        this.bulletTimer = this.game.time.now + this.FireRate;                  
+                    }                  
+                }                
+            }
             Player.prototype.update = function(){
-                //this.hola();
                 this.move();
-                
+                this.shoot();                
             }
             
     //heritage
--- src/js/code.js
+++ src/js/code.js
@@ -14,96 +14,34 @@ function preload() {
    
 }
 //Vars
-    
-    
-    /*var platforms;
-    var movement;
-    var cursors;
-    var keyW;
-    var keyS;
-    var keyA;
-    var keyD;*/
 
-
-    
-
-    
-    
-    
+   
 function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
-    //game.physics.startSystem(Phaser.Physics.ARCADE);
-
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     //  A simple background for our game
     game.add.sprite(0, 0, 'bckgrnd');
- 
 
+    var bulletPool = []
+    for(var i = 0; i<10;i++){
+        
+        bulletPool.push(new Bullet(this.game,200,200,'aid',1,'right'))
+        bulletPool[i].kill();
+        bulletPool[i].body.collideWorldBounds = false;
+        
+    }
+
+    var aid = new Bullet(this.game,200,200,'aid',1,'down');
     var head = new Basic(this.game,0,0,'head');
-    var player = new Player(this.game,400,300,'isaac',5,this.head);
+    head.body.collideWorldBounds = false;
+    var player = new Player(this.game,400,300,'isaac',5,head,bulletPool);
 
     player.addChild(head);
     head.place(-9,-35);
-
-    //player.body.collideWorldBounds = true;
-
-    var aid = new Bullet(this.game,200,200,'aid',1,'down');
     
-    
-    console.log(player);  
-    //this.game.world.addChild(aid);
-
-    if(player instanceof Phaser.Sprite)
-        console.log("Yep");
-    else console.log("Nope");
-    player.hola();
-    console.log(player.speed);
 }
 
 function update() {
     
 }
 
-/*function move(){
-    player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
-    
-
-    if(keyA.isDown || keyD.isDown || keyW.isDown ||keyS.isDown){
-        if (keyA.isDown)
-        {
-            //  Move to the left
-            player.body.velocity.x = -150;
-            player.animations.play('left');
-            head.frame = 6;
-        }
-        if (keyD.isDown)
-        {
-            //  Move to the right
-            player.body.velocity.x = 150;
-            player.animations.play('right');
-            head.frame = 2;
-        } 
-        if(keyW.isDown)
-        {
-            player.body.velocity.y = -150
-            player.animations.play('up');
-            head.frame = 4;
-    
-        }
-        if(keyS.isDown)
-        {
-            player.body.velocity.y = 150   
-            player.animations.play('up');
-            head.frame = 0;
-    
-        }
-
-    }
-    else{
-        //  Reset the players velocity (movement)
-        player.animations.stop();
-        player.frame = 0;
-        head.animations.stop();
-        head.frame = 0;
-    }
-}*/
